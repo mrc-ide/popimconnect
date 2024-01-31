90f30bf084a886246ba86b52583bbbcf46cb7253
Tini Garske <t.garske@imperial.ac.uk>
2024-01-31 13:49:20 +0000

    add tests

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 assert_vip_pop_3d <- function(x, name = deparse(substitute(x))) {
-    if(!class(x) == "vip_population_3d")
+    if(!identical(class(x), "vip_population_3d"))
         stop(sprintf("'%s' must be of class 'vip_population_3d'", name),
              call. = FALSE)
 }
